97eec755ea30ca4c28539aded748f8766deb37c7
Lakshidhaa Suresh <sllakshidhaa7@gmail.com>
2026-08-15 08:19:24 +0530

    Remove duplicate root Java files

--- FoodItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.snackpass.repository;
-
-import com.snackpass.entity.FoodItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodItemRepository
-        extends JpaRepository<FoodItem, Long> {
-}
